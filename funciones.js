459e23f2d39110b909f2b548d6cbc7ff0a4847d3
markakop <markis2802@gmail.com>
2024-02-13 19:06:28 +0100

    me vuelvo loco

--- funciones.js
+++ funciones.js
@@ -1,5 +1,5 @@
 const url = 'https://api.giphy.com/v1/gifs/search?api_key=M8VP3NcRD9N1TabYD2KBeHcmqjvfNIyJ';
-const url_subir = 'https://upload.giphy.com/v1/gifs?api_key=M8VP3NcRD9N1TabYD2KBeHcmqjvfNIyJ&';
+const url_subir = 'https://upload.giphy.com/v1/gifs?api_key=M8VP3NcRD9N1TabYD2KBeHcmqjvfNIyJ&source_image_url=';
 function aleatorio(a,l=25){
         let opciones= {
             method: 'GET'
@@ -11,5 +11,13 @@ function aleatorio(a,l=25){
 }
 
 
-
+function subir(url){
+    let opciones= {
+        method: 'POST'
+    }
+    let url_completa = url_subir + url;
+    return fetch(url_completa, opciones)
+        .then((respuesta) => respuesta.json())
+        .then((json) => json);
+}
 
